ba88e1e6da71baf6d4c2b2c6dba252580f860933
Dawit <tesfayedawit22090582@gmail.com>
2025-05-16 10:45:35 +0300

    play icon added

--- client/src/components/RowList/Row/Row.jsx
+++ client/src/components/RowList/Row/Row.jsx
@@ -20,16 +20,21 @@ function Row({ title, fetchUrl }) {
 
   return (
     <>
-      <div className="container-fluid my-4">
+      <div className="container-fluid ">
         <h4 className="text-white mb-3">{title}</h4>
         <div className="row-wrapper row flex-nowrap overflow-auto">
           {movies.map((movie) => (
             <div key={movie.id} className="col-2 flex-shrink-0">
-              <img
-                className="img-fluid h-100"
-                src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`}
-                alt={movie.title || movie.name}
-              />
+              <div className="img-container position-relative">
+                <img
+                  className="img-fluid h-100"
+                  src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`}
+                  alt={movie.title || movie.name}
+                />
+                <div className="play-icon-circle">
+                  <i className="fas fa-play play-icon"></i>
+                </div>
+              </div>
             </div>
           ))}
         </div>
