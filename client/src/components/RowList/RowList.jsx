9b89c09208040fdcdabdcce2645499e02aa5de86
Dawit <tesfayedawit22090582@gmail.com>
2025-05-20 04:28:41 +0300

    genres separate and vite config modified

--- client/src/components/RowList/RowList.jsx
+++ client/src/components/RowList/RowList.jsx
@@ -10,9 +10,12 @@ function RowList() {
         <Row title="Popular" fetchUrl={requests.fetchPopular} />
         <Row title="Top Rated" fetchUrl={requests.fetchTop} />
         <Row title="Upcoming" fetchUrl={requests.fetchUpcoming} />
+
+        <h2 className="text-center font-semibold m-4">Genres</h2>
+
         <Row title="Action" fetchUrl={requests.fetchAction} />
         <Row title="Comedy" fetchUrl={requests.fetchComedy} />
-        {/* <Row title="Romance" fetchUrl={requests.fetchRomance} /> */}
+        <Row title="Romance" fetchUrl={requests.fetchRomance} />
         <Row title="Documentary" fetchUrl={requests.fetchDocumentary} />
       </div>
     </>
